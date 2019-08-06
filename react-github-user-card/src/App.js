eb912d66de38b481d403bd0eb71c9a0f06450e03
JCRN <clemons.jason@gmail.com>
2019-08-06 14:02:56 -0700

    added axios dependency and created axios get call to Github API

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello there!</h1>
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      userData: [],
+      followers: []
+    }
+  }
+
+  getData(user) {
+    axios
+      .get(`https://api.github.com/users/${user}`)
+      .then(results => {
+        // create usr card
+        const usrCard = createCard(results.data)
+        cards.appendChild(usrCard)
+      })
+      .catch(error => {
+        alert('OOPS! Something Went Wrong :(', error)
+      })
+  }
+
+  render() {
+    console.log('user state: ', this.state)
+    return (
+      <div className="App">
+        <h1>hello there!</h1>
+      </div>
+    )
+  }
 }
 
 export default App
